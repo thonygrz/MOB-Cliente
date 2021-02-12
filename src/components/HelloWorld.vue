22f5a37ddec561df100aa119c3eafa15dfffaecd
Anthony Rodriguez <anthony81199@gmail.com>
2021-02-12 09:44:05 -0400

    Cliente listo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,200 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+  <v-container>
+    <v-row justify="center" class="text-center">
+      <v-col cols="6">
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="nombre"
+            :rules="nameRules"
+            label="Nombre del objeto"
+            required
+          ></v-text-field>
+
+          <v-btn color="success" class="mr-4" @click="crearObjeto()">
+            Crear objeto
+          </v-btn>
+          <v-btn
+            :loading="loadRestaurar"
+            dark
+            color="purple"
+            class="mr-4"
+            @click="restaurarObjetos()"
+          >
+            Restaurar
+          </v-btn>
+
+          <v-row justify="center" class="mt-3 text-center">
+            <v-col cols="4">
+              <v-btn
+                :loading="loadReplicar"
+                dark
+                color="blue"
+                class="mr-4"
+                @click="replicarObjetos()"
+              >
+                Replicar
+              </v-btn>
+            </v-col>
+            <v-col cols="4">
+              <v-select
+                :rules="selectRules"
+                required
+                :items="replicas"
+                label="Tipo de réplica"
+                v-model="tipoReplica"
+              ></v-select>
+            </v-col>
+          </v-row>
+        </v-form>
+
+        <v-data-table
+          :headers="headers"
+          :items="objetos"
+          class="elevation-1 mt-5"
         >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+          <template v-slot:item.actions="{ item }">
+            <v-icon
+              color="red"
+              small
+              class="mr-2"
+              @click="eliminarObjeto(item)"
+            >
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
+        <v-alert class="mt-3" color="blue" dark v-show="showAlertMessage">{{
+          alertMessage
+        }}</v-alert>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
-  }
+
+  data: () => ({
+    valid: true,
+    nombre: "",
+    nameRules: [
+      (v) => !!v || "El nombre es obligatorio",
+      (v) => (v && v.length <= 15) || "Name must be less than 10 characters",
+    ],
+    selectRules: [(v) => !!v || "El tipo de réplica es obligatorio"],
+    dialog: false,
+    headers: [
+      { text: "Eliminar", value: "actions", sortable: false, align: "center" },
+      { text: "Nombre", value: "nombre.$t", align: "center" },
+      { text: "Fecha", value: "fecha.$t", align: "center" },
+    ],
+    objetos: [
+      // {
+      //   nombre: {
+      //     $t: "Objetico 1",
+      //   },
+      //   fecha: {
+      //     $t: "12-03-2021",
+      //   },
+      // },
+      // {
+      //   nombre: {
+      //     $t: "Objetico 2",
+      //   },
+      //   fecha: {
+      //     $t: "12-03-2021",
+      //   },
+      // },
+    ],
+    replicas: ["COMMIT", "ABORT"],
+    tipoReplica: "COMMIT",
+    loadRestaurar: false,
+    loadReplicar: false,
+    alertMessage: "Nada",
+    showAlertMessage: false,
+  }),
+  methods: {
+    validate() {
+      return this.$refs.form.validate();
+    },
+    async getObjetos() {
+      let obj = await axios.get("objeto/consultar");
+      this.objetos = obj.data;
+      console.log("Se trae esto: ", obj.data);
+    },
+    async eliminarObjeto(item) {
+      console.log("Item: ", item);
+      await axios.delete("objeto/eliminar", { data: item });
+      this.getObjetos();
+    },
+    async crearObjeto() {
+      if (this.validate()) {
+        let date = new Date();
+        let obj = {
+          nombre: {
+            $t: this.nombre,
+          },
+          fecha: {
+            $t: `${date.getDate()}-${date.getMonth() +
+              1}-${date.getFullYear()}`,
+          },
+          accion: {
+            $t: "COMMIT",
+          },
+        };
+        await axios.post("objeto/crear", obj);
+        this.getObjetos();
+      }
+    },
+    async restaurarObjetos() {
+      this.loadRestaurar = true;
+      let obj = await axios.get("objeto/restaurar");
+      this.loadRestaurar = false;
+
+      console.log("me llega; ", obj);
+
+      if (obj.data.message === "No se restauró") {
+        this.alertMessage =
+          "No se pudo restaurar la información porque ningún servidor de réplica está activo.";
+        this.showAlertMessage = true;
+        setTimeout(() => {
+          this.showAlertMessage = false;
+        }, 5000);
+      } else {
+        if (obj.data.data.data) {
+          this.objetos = obj.data.data.data;
+        } else if (obj.data.data) this.objetos = obj.data.data;
+
+        this.alertMessage = "Se restauró la información correctamente";
+        this.showAlertMessage = true;
+        setTimeout(() => {
+          this.showAlertMessage = false;
+        }, 5000);
+      }
+
+      console.log("Se restaura: ", obj);
+    },
+    async replicarObjetos() {
+      this.loadReplicar = true;
+
+      console.log("TIPO: ", this.tipoReplica);
+      let res = await axios.post("objeto/replicar", {
+        accion: this.tipoReplica,
+      });
+      console.log("Resultado de la replicacion: ", res);
+      this.alertMessage = res.data.message;
+      this.showAlertMessage = true;
+      setTimeout(() => {
+        this.showAlertMessage = false;
+      }, 3000);
+      this.loadReplicar = false;
+    },
+  },
+
+  mounted() {
+    this.getObjetos();
+  },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
 
 Vue.config.productionTip = false;
+axios.defaults.baseURL = "http://10.2.126.74:3000/api/";
 
 new Vue({
   router,
-  render: h => h(App)
+  vuetify,
+  render: (h) => h(App),
 }).$mount("#app");
